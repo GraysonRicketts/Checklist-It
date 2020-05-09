9280d83231ae0afc8910813145d37707be64d62e
Grayson <grayson.ricketts@gmail.com>
2020-05-09 13:39:32 -0500

    setup linting

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -22,15 +22,14 @@ module.exports = {
         // Uses the recommended rules from @eslint-plugin-react
         "plugin:react/recommended",
         // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        "prettier/@typescript-eslint",
+        // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        "plugin:prettier/recommended" 
     ],
   
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-        maxLen: {
-            code: 80,
-            tabWidth: 2
-        }
+        "@typescript-eslint/explicit-function-return-type": "off",
     }
 };
--- web/src/ErrorBoundary.tsx
+++ web/src/ErrorBoundary.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
-    children: React.Component;
+  children: React.Component;
 }
 
 export class ErrorBoundary extends React.Component<Props> {
-    constructor(props: Props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-    componentDidCatch(error: Error) {
-        console.log('Error boundary: ', error.message.toString());
-    }
+  componentDidCatch(error: Error) {
+    console.log("Error boundary: ", error.message.toString());
+  }
 
-    render() {
-        const { children } = this.props;
+  render() {
+    const { children } = this.props;
 
-        return children;
-    }
+    return children;
+  }
 }
